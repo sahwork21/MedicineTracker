83f1a2530ecc789c884ba18c6fbdf0884c7f14f0
sahinto2 <sahinto2@ncsu.edu>
2024-02-28 16:26:58 -0500

    Init commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+'''This is a demo for a web application that can track medicine usage for a bunch of users
+Users contain info like name, gender, age, weight, and perscriptions
+'''
+import sqlite3
+
+from flask import Flask, session, render_template, request, g, jsonify
+if __name__ == '__main__':
+  app.run(debug = True, port = 8080)
