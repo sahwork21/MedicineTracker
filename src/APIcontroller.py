fefa005927d629a3e552fef564fe8064d04d87a4
sahwork21 <sahwork21@gmail.com>
2024-02-29 13:44:02 -0500

    [API] JSONified data by making a dict and returning a jsonified dictionary

--- src/APIcontroller.py
+++ src/APIcontroller.py
@@ -17,5 +17,17 @@ def get_user(name):
   # We will run an sqlite3 query here
   # Normally these type of queries are in another class, but this is still \
   data = repo.find_user_by_name(name)
+
+  # before we return the data we have to jsonify it with a dict
+ 
+
+  json_data = {'userID' : data[0],
+                    'username' : data[1],
+                    'age' : data[2],
+                    'gender' : data[3],
+                    'weight' : data[4],
+                    'hpc' : data[5] }
   
-  return jsonify(data)
+
+  
+  return jsonify(json_data), 200
--- src/static/user.js
+++ src/static/user.js
@@ -7,9 +7,20 @@ console.log("sdfa")
 //This is the controller for users. It has the scope variable and http requests
 app.controller("UserController", function($http, $scope, $q){
 
+  //This scope variable will contain the user's information
+  $scope.user;
   
   $scope.test = function(){
     console.log("Hello") 
+    //Send an HTTP request to get the data for Alice
+    $http.get("/user/Alice").then(function(response){
+      //Get info from our response
+      console.log(response);
+      console.log(response.data);
+      $scope.user = response.data;
+      console.log($scope.user)
+      //Flask 
+    })
   }
   $scope.test();
 });
